62239e0a2bf8bf94749f2324eb5b61269e974bc3
RavinderSinghPB <code@ParmarthBharat.com>
2022-12-23 18:13:55 +0530

    overview data

--- overview/urls.py
+++ overview/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 # from . import views
-from .views import category_data, overview
+from .views import category_data, date_catg_wise_data, date_wise_data, overview
 
 urlpatterns = [
     path('', overview, name='overview'),
     path('category_data/', category_data, name='category_data'),
+    path('date_wise/', date_wise_data, name='date_wise'),
+    path('dt_catg/', date_catg_wise_data, name='date_catg_wise'),
 ]
--- overview/views.py
+++ overview/views.py
@@ -1,15 +1,62 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import json
 
 # Create your views here.
 
-def overview(request):
-    data = [1, 2, 3]
-    return HttpResponse(data)
+def read_data_from_json(dpath='overview/static/itemsdata.json'):
 
+    with open(dpath) as f:
+        data = json.load(f)
+    return data
+
+@api_view(['GET'])
+def overview(request):
+    data = read_data_from_json()
+    
+    return Response(data)
+
+@api_view(['GET'])
 def category_data(request):
-    data = [4, 5, 6]
-    return HttpResponse(data)
+    data = read_data_from_json()
+
+    catg_data = dict()
+    for item in data:
+        if item['category']  in catg_data:
+            catg_data[item['category']].append(item)
+        else:
+            
+            catg_data[item['category']] =[item]
+
+    
+    return Response(catg_data)
+
+@api_view(['GET'])
+def date_wise_data(request):
+    date = request.GET.get('date')
+
+    data = read_data_from_json()
+    date_data = list()
+    for item in data:
+        if item['date'] == date:
+            date_data.append(item)
+    
+    return Response(date_data)
+
+@api_view(['GET'])
+def date_catg_wise_data(request):
+    date,catg = request.GET.get('date'),request.GET.get('catg')
+
+    data = read_data_from_json()
+    date_data = list()
+    for item in data:
+        if item['date'] == date and item['category'] == catg:
+            date_data.append(item)
+    
+    return Response(date_data)
 
 
 

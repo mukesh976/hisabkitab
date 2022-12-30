80076d9f171bb6b48df871b146583e00f851f8ce
RavinderSinghPB <code@ParmarthBharat.com>
2022-12-30 15:58:08 +0530

    add edit demo

--- overview/urls.py
+++ overview/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 # from . import views
-from .views import category_data, date_catg_wise_data, date_wise_data, overview
+from .views import add_item, category_data, date_catg_wise_data, date_wise_data, overview, update_item
 
 urlpatterns = [
     path('', overview, name='overview'),
     path('category_data/', category_data, name='category_data'),
     path('date_wise/', date_wise_data, name='date_wise'),
     path('dt_catg/', date_catg_wise_data, name='date_catg_wise'),
+    path('add-item/', add_item, name='add-item'),
+    path('update-item/', update_item, name='update-item')
+
 ]
--- overview/views.py
+++ overview/views.py
@@ -7,17 +7,19 @@ import json
 
 # Create your views here.
 
-def read_data_from_json(dpath='overview/static/itemsdata.json'):
+def read_data_from_json(dpath='overview/static/test.json'):
 
     with open(dpath) as f:
         data = json.load(f)
     return data
 
+
 @api_view(['GET'])
 def overview(request):
     data = read_data_from_json()
-    
+
     return Response(data)
+
 
 @api_view(['GET'])
 def category_data(request):
@@ -25,14 +27,14 @@ def category_data(request):
 
     catg_data = dict()
     for item in data:
-        if item['category']  in catg_data:
+        if item['category'] in catg_data:
             catg_data[item['category']].append(item)
         else:
-            
-            catg_data[item['category']] =[item]
 
-    
+            catg_data[item['category']] = [item]
+
     return Response(catg_data)
+
 
 @api_view(['GET'])
 def date_wise_data(request):
@@ -43,20 +45,56 @@ def date_wise_data(request):
     for item in data:
         if item['date'] == date:
             date_data.append(item)
-    
+
     return Response(date_data)
+
 
 @api_view(['GET'])
 def date_catg_wise_data(request):
-    date,catg = request.GET.get('date'),request.GET.get('catg')
+    date, catg = request.GET.get('date'), request.GET.get('catg')
 
     data = read_data_from_json()
     date_data = list()
     for item in data:
         if item['date'] == date and item['category'] == catg:
             date_data.append(item)
-    
+
     return Response(date_data)
 
 
+@api_view(['POST'])
+def add_new_item(request):
+    data = read_data_from_json()
+    new_item = {
+        "_id": len(data)+1,
+        "item": "Vimbar",
+        "category": "daily use",
+        "price": "1000",
+        "date": "2022-12-30"
+    },
+    data.append(new_item)
+    with open('overview/static/itemsdata.json', 'w') as f:
+        f.write(json.dumps(data))
+    return Response(new_item)
 
+
+@api_view(['POST'])
+def add_item(request):
+    data = read_data_from_json()
+    new_item = request.data['item']
+    data.append(new_item)
+    with open('overview/static/itemsdata.json', 'w') as f:
+        f.write(json.dumps(data))
+    return Response(new_item)
+
+@api_view(['PATCH'])
+def update_item(request):
+    data = read_data_from_json()
+    idx,upd_name = request.data['idx'],request.data['name']
+
+    item = data[idx]
+    item['item'] = upd_name
+
+    with open('overview/static/itemsdata.json', 'w') as f:
+        f.write(json.dumps(data))
+    return Response(data[idx])

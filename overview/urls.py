96fbdf0f7e7745c8575ff7af95aaf2aa6579a141
mukesh <mukesh.singh.pb@gmail.com>
2023-02-02 13:18:34 +0530

    mongodb used

--- overview/urls.py
+++ overview/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
 
 # from . import views
-from .views import add_item, category_data, date_catg_wise_data, date_wise_data, overview, update_item
-
+from .views import add_item, category_data, date_catg_wise_data,date_wise_data, overview,count_itm,update_item,cunt_itm_latest_date,date_max_itm_price,latest_date,Particular_date_data
 urlpatterns = [
     path('', overview, name='overview'),
     path('category_data/', category_data, name='category_data'),
     path('date_wise/', date_wise_data, name='date_wise'),
     path('dt_catg/', date_catg_wise_data, name='date_catg_wise'),
     path('add-item/', add_item, name='add-item'),
-    path('update-item/', update_item, name='update-item')
+    path('update-item/', update_item, name='update-item'),
+    path('cont_itm_price/',count_itm,name='count_itm'),
+    path('cont_itm_latst_date/',cunt_itm_latest_date,name='cunt_itm_latest_date'),
+    path('date_max_itm_price/',date_max_itm_price,name='date_max_itm_price'),
+    path('lat_dat/',latest_date,name='latest_date'),
+    path('partic_det_data/',Particular_date_data,name='Particular_date_data'),
 
 ]
--- overview/views.py
+++ overview/views.py
@@ -4,62 +4,121 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import json
+from datetime import datetime as dt
+from pymongo import MongoClient
 
 # Create your views here.
 
-def read_data_from_json(dpath='overview/static/test.json'):
+def read_data_from_json(dpath='overview/static/itemsdata.json'):
 
     with open(dpath) as f:
         data = json.load(f)
     return data
 
 
+# @api_view(['GET'])
+# def overview(request):
+#     data = read_data_from_json()
+
+#     return Response(data)
+
+def connect_mongo(db_name='test'):
+    client=MongoClient('mongodb+srv://test_db_user:wy1d3VNenoBmkfx5@cluster0.ohz7z5a.mongodb.net/?retryWrites=true&w=majority')
+    db=client[db_name]
+
+    return client,db
+
+
 @api_view(['GET'])
 def overview(request):
-    data = read_data_from_json()
+    client,db = connect_mongo()
+    collection = db['items']
 
-    return Response(data)
+    all_items=collection.find({},{'_id':0})
+    data=list()
+    for item in all_items:
+        data.append(item)
+
+    client.close()
+    return Response(data)    
+
+
+
+# @api_view(['GET'])
+# def category_data(request):
+#     data = read_data_from_json()
+
+#     catg_data = dict()
+#     for item in data:
+#         if item['category'] in catg_data:
+#             catg_data[item['category']].append(item)
+#         else:
+
+#             catg_data[item['category']] = [item]
+
+#     return Response(catg_data)
 
 
 @api_view(['GET'])
 def category_data(request):
-    data = read_data_from_json()
+    client=MongoClient('mongodb+srv://test_db_user:wy1d3VNenoBmkfx5@cluster0.ohz7z5a.mongodb.net/?retryWrites=true&w=majority')
+    db=client['test']
+    collection=db['items']
 
+    al_items=collection.find({},{'_id':0})
     catg_data = dict()
-    for item in data:
+    for item in al_items:
         if item['category'] in catg_data:
-            catg_data[item['category']].append(item)
+            [catg_data[item['category']]].append(item)
         else:
-
-            catg_data[item['category']] = [item]
-
-    return Response(catg_data)
+            catg_data[item['category']]=(item)
+    return Response(catg_data)            
 
 
 @api_view(['GET'])
 def date_wise_data(request):
     date = request.GET.get('date')
+    client=MongoClient('mongodb+srv://test_db_user:wy1d3VNenoBmkfx5@cluster0.ohz7z5a.mongodb.net/?retryWrites=true&w=majority')
+    db=client['test']
+    collection=db['items']
 
-    data = read_data_from_json()
+    all_items=collection.find({},{'_id':0})
     date_data = list()
-    for item in data:
-        if item['date'] == date:
+    for item in all_items:
+        if item['date']==date:
             date_data.append(item)
+    return Response(date_data)        
 
-    return Response(date_data)
+
+# @api_view(['GET'])
+# def date_catg_wise_data(request):
+#     date, catg = request.GET.get('date'), request.GET.get('catg')
+
+#     data = read_data_from_json()
+#     date_data = list()
+#     for item in data:
+#         if item['date'] == date and item['category'] == catg:
+#             date_data.append(item)
+
+#     return Response(date_data)
 
 
 @api_view(['GET'])
 def date_catg_wise_data(request):
     date, catg = request.GET.get('date'), request.GET.get('catg')
+    client=MongoClient('mongodb+srv://test_db_user:wy1d3VNenoBmkfx5@cluster0.ohz7z5a.mongodb.net/?retryWrites=true&w=majority')
+    db=client['test']
+    collection=db['items']
 
-    data = read_data_from_json()
-    date_data = list()
-    for item in data:
+    all_items=collection.find({},{'_id':0})
+    date_catg_data = list()
+    for item in all_items:
         if item['date'] == date and item['category'] == catg:
-            date_data.append(item)
+            date_catg_data.append(item)
+    return Response(date_catg_data)         
 
-    return Response(date_data)
+
+
 
 
 @api_view(['POST'])
@@ -78,14 +137,31 @@ def add_new_item(request):
     return Response(new_item)
 
 
+# @api_view(['POST'])
+# def add_item(request):
+#     data = read_data_from_json()
+#     new_item = request.data['item']
+#     data.append(new_item)
+#     with open('overview/static/itemsdata.json', 'w') as f:
+#         f.write(json.dumps(data))
+#     return Response(new_item)
+
 @api_view(['POST'])
 def add_item(request):
-    data = read_data_from_json()
+    client=MongoClient('mongodb+srv://test_db_user:wy1d3VNenoBmkfx5@cluster0.ohz7z5a.mongodb.net/?retryWrites=true&w=majority')
+    db=client['test']
+    collection=db['items']
+
     new_item = request.data['item']
-    data.append(new_item)
-    with open('overview/static/itemsdata.json', 'w') as f:
-        f.write(json.dumps(data))
-    return Response(new_item)
+    #print(new_item,"before adding to mongo")
+
+    collection.insert_one(new_item)
+    #print(new_item,"after adding mongo")
+
+    new_item["_id"]=str(new_item["_id"])
+    return Response({'added_item':new_item})
+
+
 
 @api_view(['PATCH'])
 def update_item(request):
@@ -98,3 +174,109 @@ def update_item(request):
     with open('overview/static/itemsdata.json', 'w') as f:
         f.write(json.dumps(data))
     return Response(data[idx])
+       
+
+@api_view(['GET']) # Date ke item ka maximum price.
+def date_max_itm_price(request):
+    client=MongoClient('mongodb+srv://test_db_user:wy1d3VNenoBmkfx5@cluster0.ohz7z5a.mongodb.net/?retryWrites=true&w=majority')
+    db=client['test']
+    collection=db['items']
+
+    all_items=collection.find({},{'_id':0})
+    max=0
+    itm=''
+    date= request.GET.get('date')
+    for item in all_items:
+        if item['date']==date and int(item['price'])>max:
+            max=int(item['price'])
+            itm=item['item']
+    return Response({'item':itm,'Maximum_Price':max})
+
+
+@api_view(['GET']) #Count item and price
+def count_itm(request):
+    client=MongoClient('mongodb+srv://test_db_user:wy1d3VNenoBmkfx5@cluster0.ohz7z5a.mongodb.net/?retryWrites=true&w=majority')
+    db=client['test']
+    collection=db['items']
+
+    all_items=collection.find({},{'_id':0})
+    count_dict=dict()
+    for item in all_items:
+        if item['item'] in count_dict:
+            count_dict[item['item']]['count']+=1
+            count_dict[item['item']]['price']+=int(item['price'])
+        else:
+            count_dict[item['item']]={'count':1,'price':int(item['price'])}    
+    return Response(count_dict)
+                                   
+@api_view(['GET']) #Count item_price and latest_date
+def cunt_itm_latest_date(request):
+    client=MongoClient('mongodb+srv://test_db_user:wy1d3VNenoBmkfx5@cluster0.ohz7z5a.mongodb.net/?retryWrites=true&w=majority')
+    db=client['test']
+    collection=db['items']
+
+    all_items=collection.find({},{'_id':0})
+    latest_date=dict()
+    for item in all_items:
+        if item['item'] in latest_date:
+            latest_date[item['item']]['count']+=1
+            latest_date[item['item']]['price']+=int(item['price'])
+            latest_date[item['item']]['date']=max(dt.strptime(latest_date[item['item']]['date'],'%Y-%m-%d'),
+                                                   dt.strptime(item['date'],'%Y-%m-%d')).strftime('%Y-%m-%d')
+        else:
+            latest_date[item['item']]={'count':1,'price':int(item['price']),
+                                        'date':item['date']}
+    return Response(latest_date)                                                                               
+
+
+@api_view(['GET']) # latest_date and item
+def latest_date(request):
+    client=MongoClient('mongodb+srv://test_db_user:wy1d3VNenoBmkfx5@cluster0.ohz7z5a.mongodb.net/?retryWrites=true&w=majority')
+    db=client['test']
+    collection=db['items']
+
+    all_items=collection.find({},{'_id':0})
+    for item in all_items:
+        if item['date']<=dt.strftime(dt.today(),'%Y-%m-%d'):
+            l_d={item['date']:[{'item':item['item'],'price':item['price']}]}
+    return Response(l_d)
+
+
+# @api_view(['GET']) #Particular_date_data
+# def Particular_date_data(request):
+#     date = request.GET.get('date')
+#     data = read_data_from_json()
+#     parti_det=dict()
+#     for item in data:
+#         if item['date']==date:
+#             if item['date'] in parti_det:
+#                 parti_det[item['date']]['item'].append([item['item']])
+#                 parti_det[item['date']]['total_item']+=1
+#                 parti_det[item['date']]['total_price']+=int(item['price'])
+#             else:
+#                 parti_det[item['date']]={'total_item':1,
+#                                          'total_price':int(item['price']),
+#                                         'item':[item['item']],}  
+#     return Response(parti_det)                                       
+
+
+@api_view(['GET']) #Particular_date_data
+def Particular_date_data(request):
+    date = request.GET.get('date')
+    client=MongoClient('mongodb+srv://test_db_user:wy1d3VNenoBmkfx5@cluster0.ohz7z5a.mongodb.net/?retryWrites=true&w=majority')
+    db=client['test']
+    collection=db['items']
+
+    all_items=collection.find({},{'_id':0})
+    parti_det=dict()
+    for item in all_items:
+        if item['date']==date:
+            if item['date'] in parti_det:
+                parti_det[item['date']]['item'].append([item['item']])
+                parti_det[item['date']]['total_item']+=1
+                parti_det[item['date']]['total_price']+=int(item['price'])
+            else:
+                parti_det[item['date']]={'total_item':1,
+                                          'total_price':int(item['price']),
+                                         'item':[item['item']],}
+    return Response(parti_det)                                         
